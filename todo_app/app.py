e62980500a1ada1b3710cfc027612124d17b3d02
Tom Johnston <thomas.johnston@softwire.com>
2021-11-24 10:56:49 +0000

    added mark complete buttons

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask.globals import request
-from todo_app.data.session_items import add_item, get_items
+from todo_app.data.session_items import add_item, get_item, get_items, save_item
 
 from todo_app.flask_config import Config
 
@@ -10,12 +10,25 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', items = get_items())
+    return render_template('index.html', items=get_items())
 
-@app.route('/item', methods = ['POST'])
+
+@app.route('/item', methods=['POST', 'PUT'])
 def item():
     if(request.method == 'POST'):
-        form = request.form
-        title = form.get('title')
-        add_item(title)
-        return redirect('/')
+        args = request.args
+        if(len(args) > 0):
+            id = int(args.get('id'))
+            item = get_item(id)
+            status = args.get('status')
+            updated_item = {'id': id, 'status': status, 'title': item['title']};
+            print(updated_item)
+            save_item(updated_item)
+            print(get_items())
+        else:
+            form = request.form
+            title = form.get('title')
+            add_item(title)
+    return redirect('/')
+
+
