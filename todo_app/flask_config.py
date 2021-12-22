4e4829bc0cc182585631fc20e309a8a9e2bb6658
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-22 22:40:07 +0000

    tidying code

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -11,9 +11,6 @@ class Config:
         self.organisation_id = os.getenv('BOARD_ORGANISATION_ID')
         self.board_id = os.getenv('BOARD_ID')
         
-        # self.todo_list_id = os.getenv('TODO_LIST_ID')
-        # self.done_list_id = os.getenv('DONE_LIST_ID')
-
         if not self.SECRET_KEY:
             raise ValueError(
                 "No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
