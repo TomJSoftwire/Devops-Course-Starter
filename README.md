97e2d92c89f9f21b3a53066403a6e5a885769d20
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-22 22:40:07 +0000

    added multiple lists separated by status

--- README.md
+++ README.md
@@ -66,3 +66,12 @@ You should see output similar to the following:
 ```
 
 Now visit [`http://localhost:5000/`](http://localhost:5000/) in your web browser to view the app.
+
+
+## Running the tests
+
+### Unit Tests
+
+This project uses `pytest` for unit testing, run the tests with the command `pytest` or watch the tests using `ptw`.
+
+To run individual tests append the test dir to the command used.
--- todo_app/app.py
+++ todo_app/app.py
@@ -5,28 +5,33 @@ from todo_app.data.trello_items import get_item, add_item, get_items, save_item
 from todo_app.flask_config import Config
 from todo_app.view_model import ViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
 
-@app.route('/')
-def index():
-    view_model = ViewModel(get_items())
-    return render_template('index.html', view_model=view_model)
+    @app.route('/')
+    def index():
+        view_model = ViewModel(get_items())
+        return render_template('index.html', view_model=view_model)
 
 
-@app.route('/item', methods=['POST', 'PUT'])
-def item():
-    if(request.method == 'POST'):
-        args = request.args
-        if(len(args) > 0):
-            id = args.get('id')
-            item = get_item(id)
-            status = args.get('status')
-            updated_item = {'id': id, 'status': status, 'title': item.title}
-            save_item(updated_item)
-        else:
-            form = request.form
-            title = form.get('title')
-            add_item(title)
-    return redirect('/')
+    @app.route('/item', methods=['POST', 'PUT'])
+    def item():
+        if(request.method == 'POST'):
+            args = request.args
+            if(len(args) > 0):
+                id = args.get('id')
+                item = get_item(id)
+                status = args.get('status')
+                updated_item = {'id': id, 'status': status, 'title': item.title}
+                save_item(updated_item)
+            else:
+                form = request.form
+                title = form.get('title')
+                add_item(title)
+        return redirect('/')
+
+    return app
+
+create_app()
