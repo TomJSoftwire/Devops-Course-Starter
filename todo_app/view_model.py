0f7f9d3b3019d5af2eefdd9cbdf392681beceb0a
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-22 22:40:07 +0000

    added filterd item lists to view model

--- todo_app/view_model.py
+++ todo_app/view_model.py
@@ -1,3 +1,6 @@
+from todo_app.data.item import ItemStatus
+
+
 class ViewModel:
     def __init__(self, items) -> None:
         self._items = items
@@ -5,3 +8,15 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+
+    @property
+    def todo_items(self):
+        return [item for item in self._items if item.status == ItemStatus.TO_DO]
+
+    @property
+    def done_items(self):
+        return [item for item in self._items if item.status == ItemStatus.DONE]
+
+    @property
+    def doing_items(self):
+        return [item for item in self._items if item.status == ItemStatus.DOING]
