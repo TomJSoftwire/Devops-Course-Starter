859d21316fcc3ba7926d2e7fce5c521290aeb77f
Tom Johnston <thomas.johnston@softwire.com>
2022-09-12 15:56:52 +0100

    sdfaf

--- prod-start.sh
+++ prod-start.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+echo $FLASK_APP
+echo $FLASK_ENV
+echo $SECRET_KEY
+echo $PORT
+echo $MONGO_CONNECTION_STRING
+echo $MONGO_DB_NAME
+echo $OAUTH_APP_ID
+echo $OAUTH_APP_SECRET
 poetry run gunicorn 'todo_app.app:create_app()' -b 0.0.0.0:$PORT
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -1,3 +1,4 @@
+from logging import error
 import os
 import logging
 
